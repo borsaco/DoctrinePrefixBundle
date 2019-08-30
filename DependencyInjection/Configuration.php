1ff1cb624e2f0f8588f626124469d9aa01ddc962
Alireza Seddighi <seddighi.alireza78@gmail.com>
2019-08-30 23:22:34 +0430

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,22 +4,27 @@ namespace Borsaco\DoctrinePrefixBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\HttpKernel\Kernel;
 
 class Configuration implements ConfigurationInterface
 {
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder('doctrine_prefix');
-        $rootNode = $treeBuilder->root('doctrine_prefix');
+        if (Kernel::VERSION_ID >= 40200) {
+            $builder = new TreeBuilder('doctrine_prefix');
+        } else {
+            $builder = new TreeBuilder();
+        }
+        $rootNode = \method_exists($builder, 'getRootNode') ? $builder->getRootNode() : $builder->root('doctrine_prefix');
 
-	    $rootNode
+        $rootNode
             ->children()
-                ->scalarNode('table_prefix')->defaultValue('')->end()
-                ->scalarNode('column_prefix')->defaultValue('')->end()
-                ->scalarNode('naming_strategy')->defaultValue('doctrine.orm.naming_strategy.default')->end()
+            ->scalarNode('table_prefix')->defaultValue('')->end()
+            ->scalarNode('column_prefix')->defaultValue('')->end()
+            ->scalarNode('naming_strategy')->defaultValue('doctrine.orm.naming_strategy.default')->end()
             ->end();
 
-        return $treeBuilder;
+        return $builder;
     }
 
 }
