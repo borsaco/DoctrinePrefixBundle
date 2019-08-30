d45256ece7528e5f5a70fa3c4d916bf0f1b35bd5
Alireza Seddighi <seddighi.alireza78@gmail.com>
2019-08-30 19:17:33 +0430

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Borsaco\TelegramBotApiBundle\DependencyInjection;
+namespace Borsaco\DoctrinePrefixBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
