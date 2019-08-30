d716a78c9da99eb41735eceeb1ca80deabb15d98
Alireza Seddighi <seddighi.alireza78@gmail.com>
2019-08-30 19:18:04 +0430

    Update DoctrinePrefixExtension.php

--- DependencyInjection/DoctrinePrefixExtension.php
+++ DependencyInjection/DoctrinePrefixExtension.php
@@ -8,7 +8,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class TelegramBotApiExtension extends Extension
+class DoctrinePrefixExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
@@ -17,7 +17,7 @@ class TelegramBotApiExtension extends Extension
 
         $container->setParameter('doctrine_prefix_bundle.config', $config);
 
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
+        $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . '/../Resources/config'));
         $loader->load('services.yaml');
     }
 }
